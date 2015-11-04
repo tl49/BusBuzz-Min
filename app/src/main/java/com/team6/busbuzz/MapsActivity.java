4dea95973b919cd263caffc90d1643971ee95758
Bahaness <tl492@hotmail.com>
2015-11-03 19:09:00 -0800

    Clean version with DB & bus left button added

--- app/src/main/java/com/team6/busbuzz/MapsActivity.java
+++ app/src/main/java/com/team6/busbuzz/MapsActivity.java
@@ -35,6 +35,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Marker nobelMarker;
     private Marker stop;
 
+    //Parse Objects
+    private ParseObject stopObj;
+
+    //Buttons
+    private Button emptyButton;
+    private Button spaciousButton;
+    private Button fullButton;
+    private Button busLeftButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,135 +58,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "jEzU7yAVpJXiqxwXLg3WSLTT274HJM8hoWIQxNfc", "5eAjXOb630iaYXcOpmDqLBqrh0q7i6WrLhgck0LJ");
 
-    }
+        stopObj = new ParseObject("Bus");
+        emptyButton =  (Button)findViewById(R.id.button);
+        spaciousButton =  (Button)findViewById(R.id.button2);
+        fullButton =  (Button)findViewById(R.id.button3);
+        busLeftButton = (Button)findViewById(R.id.button4);
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setMyLocationEnabled(true);
-
-        //Buttons
-        final Button emptyButton = (Button)findViewById(R.id.button);
-        final Button spaciousButton = (Button)findViewById(R.id.button2);
-        final Button fullButton = (Button)findViewById(R.id.button3);
-        //Parse Objects
-        final ParseObject stopObj = new ParseObject("Bus");
-
-
-
-        LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, true);
-
-        //User permission check for accessing last known positon
-        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED )
-        {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
-        }
-        Location location = locationManager.getLastKnownLocation(provider);
-
-        //Initial Map Camera Location
-        LatLng UCSD = new LatLng(32.879228, -117.228738);
-
-        //Bus Stops
-        LatLng Gilman = new LatLng(32.877196, -117.235784);
-        LatLng Nobel_Regent = new LatLng(32.867459, -117.219267);
-        LatLng Arriba_Regent = new LatLng(32.861505, -117.223343);
-        LatLng Lebon_Palmila = new LatLng(32.865053, -117.225050);
-        LatLng Nobel_Lebon = new LatLng(32.868551, -117.225306);
-
-
-        //Add bus pin
-        gilmanMarker = mMap.addMarker(new MarkerOptions().position(Gilman).title("Gilman Dr & Myer's")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus)));
-        regentMarker = mMap.addMarker(new MarkerOptions().position(Nobel_Regent).title("Regent Rd & Nobel Dr")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus)));
-        arribaMarker = mMap.addMarker(new MarkerOptions().position(Arriba_Regent).title("Arriba St & Regent Rd")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus)));
-        lebonMarker  = mMap.addMarker(new MarkerOptions().position(Lebon_Palmila).title("Lebon Dr & Palmila Dr")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus)));
-        nobelMarker  = mMap.addMarker(new MarkerOptions().position(Nobel_Lebon).title("Nobel Dr & Lebon Dr")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus)));
-
-
-        //Camera
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(UCSD));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                findViewById(R.id.textView3).setVisibility(View.INVISIBLE);
-                findViewById(R.id.textView4).setVisibility(View.INVISIBLE);
-                findViewById(R.id.textView5).setVisibility(View.INVISIBLE);
-                findViewById(R.id.textView6).setVisibility(View.INVISIBLE);
-                findViewById(R.id.textView7).setVisibility(View.INVISIBLE);
-                findViewById(R.id.textView8).setVisibility(View.INVISIBLE);
-                findViewById(R.id.imageView2).setVisibility(View.INVISIBLE);
-                //findViewById(R.id.button)
-                emptyButton.setVisibility(View.INVISIBLE);
-                //findViewById(R.id.button2)
-                spaciousButton.setVisibility(View.INVISIBLE);
-                //findViewById(R.id.button3)
-                fullButton.setVisibility(View.INVISIBLE);
-            }
-        });
-
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(Marker marker) {
-                findViewById(R.id.imageView2).setVisibility(View.VISIBLE);
-                findViewById(R.id.textView3).setVisibility(View.VISIBLE);
-                findViewById(R.id.textView5).setVisibility(View.VISIBLE);
-                findViewById(R.id.textView6).setVisibility(View.VISIBLE);
-                findViewById(R.id.textView7).setVisibility(View.VISIBLE);
-                findViewById(R.id.textView8).setVisibility(View.VISIBLE);
-                //findViewById(R.id.button)
-                emptyButton.setVisibility(View.VISIBLE);
-                //findViewById(R.id.button2)
-                spaciousButton.setVisibility(View.VISIBLE);
-                //findViewById(R.id.button3)
-                fullButton.setVisibility(View.VISIBLE);
-
-                if (marker.equals(gilmanMarker)) {
-                    findViewById(R.id.textView4).setVisibility(View.VISIBLE);
-                    TextView text = (TextView) findViewById(R.id.textView4);
-                    text.setText("Gilman Dr & Myer's");
-                    stop = gilmanMarker;
-                } else if (marker.equals(regentMarker)) {
-                    findViewById(R.id.textView4).setVisibility(View.VISIBLE);
-                    TextView text = (TextView) findViewById(R.id.textView4);
-                    text.setText("Regents Rd & Nobel Dr");
-                    stop = regentMarker;
-                } else if (marker.equals(arribaMarker)) {
-                    findViewById(R.id.textView4).setVisibility(View.VISIBLE);
-                    TextView text = (TextView) findViewById(R.id.textView4);
-                    text.setText("Arriba St & Regent Rd");
-                    stop = arribaMarker;
-                } else if (marker.equals(lebonMarker)) {
-                    findViewById(R.id.textView4).setVisibility(View.VISIBLE);
-                    TextView text = (TextView) findViewById(R.id.textView4);
-                    text.setText("Lebon Dr & Palmila Dr");
-                    stop = lebonMarker;
-                } else if (marker.equals(nobelMarker)) {
-                    findViewById(R.id.textView4).setVisibility(View.VISIBLE);
-                    TextView text = (TextView) findViewById(R.id.textView4);
-                    text.setText("Nobel Dr & Lebon Dr");
-                    stop = nobelMarker;
-                }
-
-                return false;
-            }
-        });
 
         emptyButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -264,6 +150,144 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+        busLeftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (stop.equals(gilmanMarker)) {
+
+                } else if (stop.equals(regentMarker)) {
+
+                } else if (stop.equals(arribaMarker)) {
+
+                } else if (stop.equals(lebonMarker)) {
+
+                } else if (stop.equals(nobelMarker)) {
+
+                }
+            }
+        });
+
+
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+
+        LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        //User permission check for accessing last known positon
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED )
+        {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        //Initial Map Camera Location
+        LatLng UCSD = new LatLng(32.879228, -117.228738);
+
+        //Bus Stops
+        LatLng Gilman = new LatLng(32.877196, -117.235784);
+        LatLng Nobel_Regent = new LatLng(32.867459, -117.219267);
+        LatLng Arriba_Regent = new LatLng(32.861505, -117.223343);
+        LatLng Lebon_Palmila = new LatLng(32.865053, -117.225050);
+        LatLng Nobel_Lebon = new LatLng(32.868551, -117.225306);
+
+
+        //Add bus pin
+        gilmanMarker = mMap.addMarker(new MarkerOptions().position(Gilman).title("Gilman Dr & Myer's")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus)));
+        regentMarker = mMap.addMarker(new MarkerOptions().position(Nobel_Regent).title("Regent Rd & Nobel Dr")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus)));
+        arribaMarker = mMap.addMarker(new MarkerOptions().position(Arriba_Regent).title("Arriba St & Regent Rd")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus)));
+        lebonMarker  = mMap.addMarker(new MarkerOptions().position(Lebon_Palmila).title("Lebon Dr & Palmila Dr")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus)));
+        nobelMarker  = mMap.addMarker(new MarkerOptions().position(Nobel_Lebon).title("Nobel Dr & Lebon Dr")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.bus)));
+
+
+        //Camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(UCSD));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(14));
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                findViewById(R.id.textView3).setVisibility(View.INVISIBLE);
+                findViewById(R.id.textView4).setVisibility(View.INVISIBLE);
+                findViewById(R.id.textView5).setVisibility(View.INVISIBLE);
+                findViewById(R.id.textView6).setVisibility(View.INVISIBLE);
+                findViewById(R.id.textView7).setVisibility(View.INVISIBLE);
+                findViewById(R.id.textView8).setVisibility(View.INVISIBLE);
+                findViewById(R.id.imageView2).setVisibility(View.INVISIBLE);
+                emptyButton.setVisibility(View.INVISIBLE);
+                spaciousButton.setVisibility(View.INVISIBLE);
+                fullButton.setVisibility(View.INVISIBLE);
+                busLeftButton.setVisibility(View.INVISIBLE);
+
+            }
+        });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                findViewById(R.id.imageView2).setVisibility(View.VISIBLE);
+                findViewById(R.id.textView3).setVisibility(View.VISIBLE);
+                findViewById(R.id.textView5).setVisibility(View.VISIBLE);
+                findViewById(R.id.textView6).setVisibility(View.VISIBLE);
+                findViewById(R.id.textView7).setVisibility(View.VISIBLE);
+                findViewById(R.id.textView8).setVisibility(View.VISIBLE);
+                emptyButton.setVisibility(View.VISIBLE);
+                spaciousButton.setVisibility(View.VISIBLE);
+                fullButton.setVisibility(View.VISIBLE);
+                busLeftButton.setVisibility(View.VISIBLE);
+
+                if (marker.equals(gilmanMarker)) {
+                    findViewById(R.id.textView4).setVisibility(View.VISIBLE);
+                    TextView text = (TextView) findViewById(R.id.textView4);
+                    text.setText("Gilman Dr & Myer's");
+                    stop = gilmanMarker;
+                } else if (marker.equals(regentMarker)) {
+                    findViewById(R.id.textView4).setVisibility(View.VISIBLE);
+                    TextView text = (TextView) findViewById(R.id.textView4);
+                    text.setText("Regents Rd & Nobel Dr");
+                    stop = regentMarker;
+                } else if (marker.equals(arribaMarker)) {
+                    findViewById(R.id.textView4).setVisibility(View.VISIBLE);
+                    TextView text = (TextView) findViewById(R.id.textView4);
+                    text.setText("Arriba St & Regent Rd");
+                    stop = arribaMarker;
+                } else if (marker.equals(lebonMarker)) {
+                    findViewById(R.id.textView4).setVisibility(View.VISIBLE);
+                    TextView text = (TextView) findViewById(R.id.textView4);
+                    text.setText("Lebon Dr & Palmila Dr");
+                    stop = lebonMarker;
+                } else if (marker.equals(nobelMarker)) {
+                    findViewById(R.id.textView4).setVisibility(View.VISIBLE);
+                    TextView text = (TextView) findViewById(R.id.textView4);
+                    text.setText("Nobel Dr & Lebon Dr");
+                    stop = nobelMarker;
+                }
+
+                return false;
+            }
+        });
 
     }
 
